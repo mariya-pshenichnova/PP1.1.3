a9df3d10d00576becc93f434e9f53d7499433944
mariya-pshenichnova <m.pshe@yandex.ru>
2022-06-24 10:32:26 +0300

    save +

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -37,7 +37,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try (PreparedStatement preparedStatement = connection
-                .prepareStatement("INSERT INTO + users(name, " +
+                .prepareStatement("INSERT INTO users(name, " +
                         "lastName, age) VALUES(?, ?, ?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
